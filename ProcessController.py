ebac37fcffa320ec321bf04823ab60158d9e7a24
proweb <verevka532@gmail.com>
2020-11-24 15:40:02 +0300

    structure was simplified

--- ProcessController.py
+++ ProcessController.py
@@ -1,6 +1,5 @@
 import os
 
-
 import face_alignment
 import pandas as pd
 import torch
@@ -22,29 +21,9 @@ class ProcessController(object):
         self.interval = trimInterval
         pd.options.mode.chained_assignment = None
 
-    def loadAll(self, rawVideoDir="./dataset/video/raw/"):
-        for i in range(len(self.loadList)):
-            try:
-                url = self.loadList["URL"][i]
-                videoLoader = VideoLoader(url, rawVideoDir=rawVideoDir)
-                videoLoader.load()
-                self.loadList["STATUS"][i] = "LOADED"
-            except Exception as e:
-                self.loadList["STATUS"][i] = "ERROR"
-                print('Failed to upload to ftp: ' + str(e))
-                continue
-
-    def trimAll(self, rawVideoDir="./dataset/video/raw/"):
-        dir_path = rawVideoDir
-        dir = os.listdir(dir_path)
-        for elem in dir:
-            if elem.count(".mp4") == 1:
-                videoTrimmer = VideoTrimmer(dir_path + elem, interval=self.interval)
-                videoTrimmer.trim()
-
     def load_and_trim_batch(self, batch_urls, rawVideoDir):
         for url in batch_urls:
-            videoLoader = VideoLoader(url, rawVideoDir=rawVideoDir, batch_mode=True)
+            videoLoader = VideoLoader(url, rawVideoDir=rawVideoDir)
             videoLoader.load()
         dir_path = rawVideoDir
 
@@ -59,29 +38,3 @@ class ProcessController(object):
             if elem.count(".mp4") == 1:
                 videoTrimmer = VideoTrimmer(dir_path + elem, interval=self.interval)
                 videoTrimmer.trim()
-
-    @staticmethod
-    def checkAll(rawVideoDir="./dataset/video/raw/"):
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        #model = InceptionResnetV1(pretrained='vggface2').eval()
-        device_str = 'cpu' if device == torch.device('cpu') else 'cuda:0'
-#         kp_detector = load_checkpoints(config_path='/content/Data-parser/vox-256.yaml',
-#                             checkpoint_path='/content/gdrive/My Drive/first-order-motion-model/vox-cpk.pth.tar')
-        fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cuda:0')
-        raw_dir_path = rawVideoDir
-        raw_dir = os.listdir(raw_dir_path)
-        for elem in raw_dir:
-            path = raw_dir_path + elem
-            if path.count(".mp4") == 0:
-                check_dir = path
-                videoChecker = VideoChecker(check_dir)
-                videoChecker.check(fa, device, None, KP_d=None)
-
-    def process(self):
-
-        rawVideoDir = "/content/gdrive/My Drive/dataset/video/raw/"
-        self.loadAll(rawVideoDir)
-        print("Loaded, trimming!")
-        self.trimAll(rawVideoDir)
-        print("Trimmed, checking!")
-        self.checkAll(rawVideoDir)
--- VideoLoader.py
+++ VideoLoader.py
@@ -9,13 +9,12 @@ from pafy import pafy
 
 class VideoLoader(object):
 
-    def __init__(self, url, rawVideoDir, batch_mode=False):
+    def __init__(self, url, rawVideoDir):
         self.url = url
         self.youtubeObj = pafy.new(url)
         self.rawVideoDir = rawVideoDir
         self.ID = self.youtubeObj.videoid
         self.rawVideo = self.youtubeObj.getbest(preftype="mp4")
-        self.batch_mode = batch_mode
 
         streams = self.youtubeObj.streams
         for s in streams:
@@ -24,35 +23,14 @@ class VideoLoader(object):
         if not os.path.exists(rawVideoDir):
             os.makedirs(rawVideoDir)
 
-        if self.batch_mode:
-            self.load_batch = True
-
     def load(self):
-        if self.batch_mode:
-            self.load_one_batch()
+        print("Loading file...")
+        videoName = self.youtubeObj.title.replace("/", "_") + ".mp4"
+        if (not os.path.exists(self.rawVideoDir + self.ID + ".mp4")) and (
+                not os.path.exists(self.rawVideoDir + self.ID)):
+            self.rawVideo.download(filepath=self.rawVideoDir)
         else:
-            print("Loading file...")
-            videoName = self.youtubeObj.title.replace("/", "_") + ".mp4"
-            if (not os.path.exists(self.rawVideoDir + self.ID + ".mp4")) and (
-            not os.path.exists(self.rawVideoDir + self.ID)):
-                self.rawVideo.download(filepath=self.rawVideoDir)
-            else:
-                print("Video already loaded!")
-            if os.path.exists(self.rawVideoDir + videoName):
-                os.rename(self.rawVideoDir + videoName, self.rawVideoDir + self.ID + ".mp4")
-            print("Done!")
-
-    def load_one_batch(self):
-        if self.load_batch:
-            print("Loading file...")
-            videoName = self.youtubeObj.title.replace("/", "_") + ".mp4"
-            if (not os.path.exists(self.rawVideoDir + self.ID + ".mp4")) and (
-            not os.path.exists(self.rawVideoDir + self.ID)):
-                self.rawVideo.download(filepath=self.rawVideoDir)
-            else:
-                print("Video already loaded!")
-            if os.path.exists(self.rawVideoDir + videoName):
-                os.rename(self.rawVideoDir + videoName, self.rawVideoDir + self.ID + ".mp4")
-            print("Done!")
-        else:
-            print("Batch was already loaded!")
+            print("Video already loaded!")
+        if os.path.exists(self.rawVideoDir + videoName):
+            os.rename(self.rawVideoDir + videoName, self.rawVideoDir + self.ID + ".mp4")
+        print("Done!")
